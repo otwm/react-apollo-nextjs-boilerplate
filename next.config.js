a2755e93a7a3dc7b0a473058054366016fd23021
dong o kim <otwmoo@gmail.com>
2022-01-29 22:31:37 +0900

    feat: install next js

---
+++ next.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  webpack: (config, { defaultLoaders, webpack }) => {
+    // config.plugins.push(new webpack.IgnorePlugin(/\/__tests__\//))
+
+    // config.module.rules.push({
+    //   test: /\.graphql$/,
+    //   exclude: /node_modules/,
+    //   use: [
+    //     defaultLoaders.babel,
+    //     { loader: 'graphql-let/loader' },
+    //   ],
+    // })
+
+    config.module.rules.push({
+      test: /\.graphqls$/,
+      exclude: /node_modules/,
+      use: ['graphql-let/schema/loader'],
+    })
+
+    config.module.rules.push({
+      test: /\.ya?ml$/,
+      type: 'json',
+      use: 'yaml-loader',
+    })
+
+    return config
+  }
+}
